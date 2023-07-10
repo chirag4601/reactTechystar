17aae72572216f213accde87f3c18e2a90b6d0af
chirag4601 <chirag4601@gmail.com>
2023-07-10 15:59:26 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+function App() {
+  return (
+    <div> 
+      <Router>
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
